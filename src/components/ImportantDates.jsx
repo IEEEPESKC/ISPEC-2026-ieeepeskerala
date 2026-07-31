d5962ac2ce73ad987573e2c59810fd2f48788f01
Sarath A <itsarath9090@gmail.com>
2026-07-31 22:23:55 +0530

    Full paper submission

--- src/components/ImportantDates.jsx
+++ src/components/ImportantDates.jsx
@@ -8,7 +8,7 @@ const ImportantDates = () => {
             status: 'active'
         },
         
-        { label: 'Full Paper Submission Deadline', date: '31 July 2026', status: 'active' },
+        { label: 'Full Paper Submission Deadline', date: '15 August 2026', status: 'active' },
         { label: 'Notification of Acceptance', date: '30 September 2026', status: 'future' },
         { label: 'Final Paper Submission', date: '15 October 2026', status: 'future' },
         { label: 'Registration Deadline', date: '1 November 2026', status: 'future' },
