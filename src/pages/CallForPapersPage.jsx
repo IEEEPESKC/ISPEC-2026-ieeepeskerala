00aac31193bf368b0bc2dacb1b693a79aa5144af
Sarath A <itsarath9090@gmail.com>
2026-07-31 22:40:22 +0530

    marquee update (#27)
    * marquee update

--- src/pages/CallForPapersPage.jsx
+++ src/pages/CallForPapersPage.jsx
@@ -220,7 +220,7 @@ const CallForPapersPage = () => {
             </div>
             <div className="news-ticker-track">
                 <div className="news-ticker-content">
-                    <span>Paper Submission Deadline extended to July 31</span>
+                    <span>Paper Submission Deadline extended to August 15</span>
                     <span>All accepted Papers will be published in IEEE Xplore</span>
                     <span>Paper Submission Deadline extended to July 31</span>
                     <span>All accepted Papers will be published in IEEE Xplore</span>
